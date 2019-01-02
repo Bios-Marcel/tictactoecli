9af52d9628b8ad7494d2b59341b0c83c085dda24
Marcel Schramm <marceloschr@googlemail.com>
2019-01-02 02:56:58 +0100

    Add tests for a tie case

---
+++ tests/ties_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Bios-Marcel/tictactoecli/internal"
+)
+
+func TestTie(t *testing.T) {
+	game := internal.NewTicTacToe()
+
+	//Start
+	game.NumEntered(0)
+
+	//Turns
+	game.NumEntered(1)
+	game.NumEntered(2)
+	game.NumEntered(3)
+	game.NumEntered(5)
+	game.NumEntered(4)
+	game.NumEntered(7)
+	game.NumEntered(6)
+	game.NumEntered(9)
+	game.NumEntered(8)
+
+	winner := *game.GetWinner()
+	if winner != internal.None {
+		t.Errorf("The winner was incorrect, it was %s, but should have been %s", winner, internal.None)
+	}
+}
